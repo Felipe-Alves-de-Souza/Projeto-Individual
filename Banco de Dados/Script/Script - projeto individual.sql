feb6733587cf766d204dac510e206d642d813da4
Felipe-Alves-de-Souza <felipe.ssouza@sptech.school>
2025-11-15 17:22:48 -0300

    adição da modelagem e script Banco-de-dados

---
+++ Banco de Dados/Script/Script - projeto individual.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE Demon;
+USE Demon;
+
+CREATE TABLE Usuario (
+    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(45) NOT NULL,
+    Email VARCHAR(45) NOT NULL UNIQUE,
+    Senha VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE Pergunta_avaliativa (
+    id_pergunta INT AUTO_INCREMENT PRIMARY KEY,
+    gostou_anime TINYINT(1) NOT NULL, 
+    comentario VARCHAR(200),     
+    data_resposta DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    Usuario_idUsuario INT NOT NULL,    
+    FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario(idUsuario)
+);
+
