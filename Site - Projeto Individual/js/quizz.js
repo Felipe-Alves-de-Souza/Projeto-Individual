d12631f8719788e7e368f16da2586e303d57d369
Felipe-Alves-de-Souza <felipe.ssouza@sptech.school>
2025-11-16 15:22:48 -0300

    atualização na página de avaliação e calculadora

---
+++ Site - Projeto Individual/js/quizz.js
@@ -0,0 +1,46 @@
+var perguntas = []; 
+let pontuacao = 0;    
+
+var hashiras = [
+    "Giyu Tomioka (Água)",
+    "Kyojuro Rengoku (Chama)",
+    "Tengen Uzui (Som)"
+];
+
+var descricoes = [
+    "Você é calmo, estratégico e mantém a compostura — assim como o Hashira da Água.",
+    "Você é intenso, determinado e corajoso — como o Hashira das Chamas!",
+    "Você é rápido, imprevisível e estiloso — igual ao Hashira do Som."
+];
+
+function calcularHashira() {
+
+    pontuacao = 0;
+    perguntas.length = 0;
+
+    // acessando selects DIRECTAMENTE pelos IDs globais
+    perguntas.push(Number(p1.value));
+    perguntas.push(Number(p2.value));
+    perguntas.push(Number(p3.value));
+
+    // soma as respostas
+    for (let i = 0; i < perguntas.length; i++) {
+        pontuacao += perguntas[i];
+    }
+
+    let indice = 0;
+
+    if (pontuacao <= 4) {
+        indice = 0;
+    } 
+    else if (pontuacao <= 7) {
+        indice = 1;
+    } 
+    else {
+        indice = 2;
+    }
+
+    
+    resultado.innerHTML = "Você combina com: " + hashiras[indice];
+    descricao.innerHTML = `${descricoes[indice]} `;
+}
